a827d2f47d3468f37512a3270ea1511f058c576d
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-01 14:46:16 +0300

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task";
+
+// Sample task data to be displayed in the TaskList component
+const tasksData = [
+  { id: 1, text: "Complete homework", category: "Study" },
+  { id: 2, text: "Go grocery shopping", category: "Personal" },
+  { id: 3, text: "Exercise for 30 minutes", category: "Health" },
+];
 
 function TaskList() {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksData.map((task) => (
+        <Task key={task.id} text={task.text} category={task.category} />
+      ))}
     </div>
   );
 }
