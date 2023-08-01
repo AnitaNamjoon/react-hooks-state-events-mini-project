5460521ccbb47cbd48caf59dd928acc418ec2c1b
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-01 14:47:24 +0300

    Update CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,33 @@
 import React from "react";
+function CategoryFilter({ categories, selectedCategory, onCategoryFilter }) {
 
-function CategoryFilter() {
+  const handleCategoryClick = (category) => {
+    onCategoryFilter(category);
+  };
+
   return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+    
+    <div className="category-filter">
+    
+      <button
+        className={selectedCategory === "All" ? "selected" : ""}
+        
+        onClick={() => handleCategoryClick("All")}
+      >
+        All
+      </button>
+     
+      {categories.map((category) => (
+        <button
+          key={category}
+          
+          className={selectedCategory === category ? "selected" : ""}
+          
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
